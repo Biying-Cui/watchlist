8e71afb7019c790217a75f12fe01c7abaef2aee8
CBB <cuibiyingtju@gmail.com>
2021-09-03 16:40:32 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ app = Flask(__name__) #通过实例化这个类，创建一个程序对象app
 # 	# return '欢迎来到CBB的 Watchlist!'
 # 	return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
+
 # 写入了一个 SQLALCHEMY_DATABASE_URI 变量来告诉 SQLAlchemy 数据库连接地址：
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
@@ -54,24 +55,24 @@ def test_url_for():
 
 
 
-@app.route('/')
-def index():
-	# name = 'CBB'
-	# movies = [
-	# {'title': 'My Neighbor Totoro', 'year': '1988'},
-	# {'title': 'Dead Poets Society', 'year': '1989'},
-	# {'title': 'A Perfect World', 'year': '1993'},
-	# {'title': 'Leon', 'year': '1994'},
-	# {'title': 'Mahjong', 'year': '1996'},
-	# {'title': 'Swallowtail Butterfly', 'year': '1996'},
-	# {'title': 'King of Comedy', 'year': '1999'},
-	# {'title': 'Devils on the Doorstep', 'year': '1999'},
-	# {'title': 'WALL-E', 'year': '2008'},
-	# {'title': 'The Pork of Music', 'year': '2012'},
-	# ]
-	user = User.query.first()
-	movies = Movie.query.all()
-	return render_template('index.html', user=user, movies=movies)
+# @app.route('/')
+# def index():
+# 	# name = 'CBB'
+# 	# movies = [
+# 	# {'title': 'My Neighbor Totoro', 'year': '1988'},
+# 	# {'title': 'Dead Poets Society', 'year': '1989'},
+# 	# {'title': 'A Perfect World', 'year': '1993'},
+# 	# {'title': 'Leon', 'year': '1994'},
+# 	# {'title': 'Mahjong', 'year': '1996'},
+# 	# {'title': 'Swallowtail Butterfly', 'year': '1996'},
+# 	# {'title': 'King of Comedy', 'year': '1999'},
+# 	# {'title': 'Devils on the Doorstep', 'year': '1999'},
+# 	# {'title': 'WALL-E', 'year': '2008'},
+# 	# {'title': 'The Pork of Music', 'year': '2012'},
+# 	# ]
+# 	user = User.query.first()
+# 	movies = Movie.query.all()
+# 	return render_template('index.html', user=user, movies=movies)
 
 @app.cli.command()
 @click.option('--drop', is_flag = True, help = 'Create after drop')
@@ -123,13 +124,23 @@ def forge():
 
 
 #404错误处理函数
-@app.errorhandler(404) # 传入要处理的错误代码
-def page_not_found(e): # 接受异常对象作为参数
-	user = User.query.first()
-	return render_template('404.html', user=user), 404 # 返回模板和状态码
+# @app.errorhandler(404) # 传入要处理的错误代码
+# def page_not_found(e): # 接受异常对象作为参数
+# 	user = User.query.first()
+# 	return render_template('404.html', user=user), 404 # 返回模板和状态码
 
 #对于多个模板内都需要使用的变量，我们可以使用 app.context_processor 装饰器注册一个模板上下文处理函数
 @app.context_processor
 def inject_user(): # 函数名可以随意修改
 	user = User.query.first()
 	return dict(user=user) # 需要返回字典，等同于return {'user': user}
+
+#以删除 404 错误处理函数和主页视图函数中的 user 变量定义，并删除在 render_template() 函数里传入的关键字参数
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.route('/')
+def index():
+	movies = Movie.query.all()
+	return render_template('index.html', movies=movies)
