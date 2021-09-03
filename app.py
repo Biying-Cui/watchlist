73f6c0aedfa429e95a09b8af1631e884b85bb8d9
CBB <cuibiyingtju@gmail.com>
2021-09-03 11:02:47 +0800

    Add index page

--- app.py
+++ app.py
@@ -38,15 +38,16 @@ def test_url_for():
 def index():
 	name = 'CBB'
 	movies = [
-{'title': 'My Neighbor Totoro', 'year': '1988'},
-{'title': 'Dead Poets Society', 'year': '1989'},
-{'title': 'A Perfect World', 'year': '1993'},
-{'title': 'Leon', 'year': '1994'},
-{'title': 'Mahjong', 'year': '1996'},
-{'title': 'Swallowtail Butterfly', 'year': '1996'},
-{'title': 'King of Comedy', 'year': '1999'},
-{'title': 'Devils on the Doorstep', 'year': '1999'},
-{'title': 'WALL-E', 'year': '2008'},
-{'title': 'The Pork of Music', 'year': '2012'},
-]
+	{'title': 'My Neighbor Totoro', 'year': '1988'},
+	{'title': 'Dead Poets Society', 'year': '1989'},
+	{'title': 'A Perfect World', 'year': '1993'},
+	{'title': 'Leon', 'year': '1994'},
+	{'title': 'Mahjong', 'year': '1996'},
+	{'title': 'Swallowtail Butterfly', 'year': '1996'},
+	{'title': 'King of Comedy', 'year': '1999'},
+	{'title': 'Devils on the Doorstep', 'year': '1999'},
+	{'title': 'WALL-E', 'year': '2008'},
+	{'title': 'The Pork of Music', 'year': '2012'},
+	]
+	
 	return render_template('index.html',name=name,movies=movies)
